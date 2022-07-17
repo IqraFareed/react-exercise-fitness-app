06fe7108af8a9f53b07531fb2cc185298664aa52
IqraFareed <iqrafareed56@gmail.com>
2022-07-17 07:42:17 +0500

    fetch-data

--- src/components/SearchExercies.js
+++ src/components/SearchExercies.js
@@ -1,11 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 const SearchExercies = () => {
   const [search, setSearch] = useState("");
-  const handleSearch = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async () => {
     if (search) {
-      // const exercisesData = await fetchData()
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+      const SearchExercie = exercisesData.filter((exercise) => {
+        exercise.name.toLowerCase.includes(search) ||
+          exercise.target.toLowerCase.includes(search) ||
+          exercise.equipment.toLowerCase.includes(search) ||
+          exercise.bodyPart.toLowerCase.includes(search);
+      });
+      setSearch("");
+      setExercises(SearchExercie);
     }
   };
   return (
@@ -32,7 +56,7 @@ const SearchExercies = () => {
           height="76px"
           value={search}
           onChange={(e) => {
-            setSearch(e.target.value.toLocaleLowerCase);
+            setSearch(e.target.value.toLowerCase());
           }}
           placeholder="Search Exercise"
           type="text"
@@ -53,6 +77,9 @@ const SearchExercies = () => {
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        {/* <HorizontalScrollbar data={bodyParts} /> */}
       </Box>
     </Stack>
   );
---
+++ src/utils/fetchData.js
@@ -0,0 +1,13 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "db16f826f0mshddeba93718be18dp1acdbcjsn16057dd36621",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
